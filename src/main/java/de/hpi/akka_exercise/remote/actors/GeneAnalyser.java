b9f763eca4a90f46b50212801c51c7672ac00704
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-11-29 19:34:43 +0100

    Adds todo for shared superclass

--- src/main/java/de/hpi/akka_exercise/remote/actors/GeneAnalyser.java
+++ src/main/java/de/hpi/akka_exercise/remote/actors/GeneAnalyser.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+// TODO: shared superclass
 public class GeneAnalyser extends AbstractLoggingActor {
-
 
     @NoArgsConstructor
     @AllArgsConstructor
