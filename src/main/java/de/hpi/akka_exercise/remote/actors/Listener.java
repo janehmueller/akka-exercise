71bf96cd24b765152d996aeadaf24d259fd9967c
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-12-09 18:15:39 +0100

    Adds student setter and shutdown message to listener

--- src/main/java/de/hpi/akka_exercise/remote/actors/Listener.java
+++ src/main/java/de/hpi/akka_exercise/remote/actors/Listener.java
@@ -1,8 +1,10 @@
 package de.hpi.akka_exercise.remote.actors;
 
 import akka.actor.AbstractLoggingActor;
+import akka.actor.PoisonPill;
 import akka.actor.Props;
 import de.hpi.akka_exercise.StudentList;
+import de.hpi.akka_exercise.messages.ShutdownMessage;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +15,12 @@ public class Listener extends AbstractLoggingActor {
     private StudentList studentList;
 
     public static Props props() { return Props.create(Listener.class); }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class StudentMessage implements Serializable {
+        private StudentList students;
+    }
 
     @NoArgsConstructor
     @AllArgsConstructor
@@ -35,9 +43,15 @@ public class Listener extends AbstractLoggingActor {
     @Override
     public Receive createReceive() {
         return receiveBuilder()
+            .match(StudentMessage.class, this::handle)
             .match(LogPasswordMessage.class, this::handle)
+            .match(ShutdownMessage.class, this::handle)
             .matchAny(object -> this.log().error(this.getClass().getName() + " received unknown message: " + object.toString()))
             .build();
+    }
+
+    private void handle(StudentMessage message) {
+        this.studentList = message.students;
     }
 
     private void handle(LogPasswordMessage message) {
@@ -47,5 +61,10 @@ public class Listener extends AbstractLoggingActor {
             studentList.updateStudentPassword(index, password);
             this.log().info("Cracked password for student {} ({}): {}", index, studentList.getStudent(index).getName() , password);
         }
+    }
+
+    private void handle(ShutdownMessage message) {
+        // TODO write students to disk
+        this.getSelf().tell(PoisonPill.getInstance(), this.getSelf());
     }
 }
